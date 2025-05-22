457652e817bf4b679f72018bb01d5cae473543b7
alan <652732310@qq.com>
2025-05-22 10:27:53 +0800

    fix gas limit issue
    TODO fix verify

--- programs/groth/src/lib.rs
+++ programs/groth/src/lib.rs
@@ -12,8 +12,20 @@ pub mod groth {
     use super::*;
 
     pub fn verify(ctx: Context<Verify>, args: VerifyArgs) -> Result<()> {
+        let mut args = args;
+        for b in [
+            31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 22, 21,
+            20, 19, 18, 17, 16, 15, 14, 13, 12, 12,
+        ] {
+            let mut bytes: [u8; 32] = [0; 32];
+            bytes[31] = b as u8;
+            args.public_inputs.push(bytes);
+        }
+
+        msg!("step 1");
         let pi = args.public_inputs.try_into().unwrap();
-        let mut verifier = Groth16Verifier::<'_, {VERIFYINGKEY.nr_pubinputs-1}>::new(
+        msg!("step 2");
+        let mut verifier = Groth16Verifier::<'_, { VERIFYINGKEY.nr_pubinputs - 1 }>::new(
             &args.proof_a,
             &args.proof_b,
             &args.proof_c,
@@ -21,7 +33,9 @@ pub mod groth {
             &VERIFYINGKEY,
         )
         .unwrap();
+    msg!("step 3");
         verifier.verify().unwrap();
+        msg!("step 4");
         Ok(())
     }
 }
