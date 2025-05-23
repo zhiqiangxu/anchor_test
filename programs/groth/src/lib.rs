3eab1b7244e20efd9381b6f178bd39023304cff9
alan <652732310@qq.com>
2025-05-23 11:00:57 +0800

    deal with malleability but stil fails

--- programs/groth/src/lib.rs
+++ programs/groth/src/lib.rs
@@ -5,7 +5,27 @@ declare_id!("HkFStTqPEnfq4R9UiymSmqhz4gLMydQ35CvBDduHSuPw");
 mod instructions;
 pub use instructions::*;
 
+use ark_serialize::{CanonicalDeserialize, CanonicalSerialize, Compress, Validate};
 use groth16_solana::groth16::Groth16Verifier;
+use std::ops::Neg;
+
+type G1 = ark_bn254::g1::G1Affine;
+type G2 = ark_bn254::g2::G2Affine;
+
+pub fn to_hex_string(bytes: &[u8]) -> String {
+    let strs: Vec<String> = bytes.iter().map(|b| format!("{:02X}", b)).collect();
+    strs.join("")
+}
+
+fn change_endianness(bytes: &[u8]) -> Vec<u8> {
+    let mut vec = Vec::new();
+    for b in bytes.chunks(32) {
+        for byte in b.iter().rev() {
+            vec.push(*byte);
+        }
+    }
+    vec
+}
 
 #[program]
 pub mod groth {
@@ -21,19 +41,44 @@ pub mod groth {
             bytes[31] = b as u8;
             args.public_inputs.push(bytes);
         }
-
+        // msg!("proof_a:{}", to_hex_string(&args.proof_a));
+        // msg!("proof_b:{}", to_hex_string(&args.proof_b));
+        // msg!("proof_c:{}", to_hex_string(&args.proof_c));
+        // for (i, input) in args.public_inputs.iter().enumerate() {
+        //     msg!("pi:{}, input:{:?}", i, &args.public_inputs[i]);
+        // }
         msg!("step 1");
         let pi = args.public_inputs.try_into().unwrap();
         msg!("step 2");
+
+        let proof_a: G1 = G1::deserialize_with_mode(
+            &*[&change_endianness(&args.proof_a), &[0u8][..]].concat(),
+            Compress::No,
+            Validate::Yes,
+        )
+        .unwrap();
+        let mut proof_a_neg = [0u8; 65];
+        proof_a
+            .neg()
+            .x
+            .serialize_with_mode(&mut proof_a_neg[..32], Compress::No)
+            .unwrap();
+        proof_a
+            .neg()
+            .y
+            .serialize_with_mode(&mut proof_a_neg[32..], Compress::No)
+            .unwrap();
+
+        let proof_a = change_endianness(&proof_a_neg[..64]).try_into().unwrap();
         let mut verifier = Groth16Verifier::<'_, { VERIFYINGKEY.nr_pubinputs - 1 }>::new(
-            &args.proof_a,
+            &proof_a,
             &args.proof_b,
             &args.proof_c,
             &pi,
             &VERIFYINGKEY,
         )
         .unwrap();
-    msg!("step 3");
+        msg!("step 3");
         verifier.verify().unwrap();
         msg!("step 4");
         Ok(())
